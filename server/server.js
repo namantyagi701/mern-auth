243384b644c62b0b458180f15d26b6cc2d0f27d0
Naman Tyagi <namantyagi98665@gmail.com>
2026-01-15 05:39:55 +0530

    Make Connection

--- server/server.js
+++ server/server.js
@@ -1,25 +1,39 @@
 import express from "express";
 import cors from "cors";
-import 'dotenv/config';
+import "dotenv/config";
 import cookieParser from "cookie-parser";
 import connectDB from "./config/mongodb.js";
 import authRouter from "./routes/authRoutes.js";
 import userRouter from "./routes/userRouter.js";
 
 const app = express();
-const port = process.env.PORT || 4000
+const port = process.env.PORT || 4000;
+
 connectDB();
 
-const allowedOrigins = ["http://localhost:5173", "http://localhost:5174" , "https://mern-authentication-ten-inky.vercel.app/"]
-app.use(express.json());
-app.use(cookieParser());
+const allowedOrigins = [
+  "http://localhost:5173",
+  "http://localhost:5174",
+  "https://mern-authentication-ten-inky.vercel.app"
+];
+
 app.use(cors({
-  origin: "https://mern-authentication-ten-inky.vercel.app", 
+  origin: function (origin, callback) {
+    if (!origin || allowedOrigins.includes(origin)) {
+      callback(null, true);
+    } else {
+      callback(new Error("Not allowed by CORS"));
+    }
+  },
   credentials: true
 }));
 
-app.get('/' , (req , res) => res.send("API Working")); //test
-app.use('/api/auth' , authRouter)
-app.use('/api/user' , userRouter)
+app.use(express.json());
+app.use(cookieParser());
 
-app.listen(port , () => console.log(`Server started on PORT:${port}`));
+app.get("/", (req, res) => res.send("API Working"));
+
+app.use("/api/auth", authRouter);
+app.use("/api/user", userRouter);
+
+app.listen(port, () => console.log(`Server started on PORT:${port}`));
